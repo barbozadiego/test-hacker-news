a12a9d50f7c0a4837c5d3387322f30794a89a591
Diego Barboza <barbozadiego1994@gmail.com>
2022-01-15 17:41:32 -0400

    Directory structures organized and ready to start programming

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,24 @@
+// import { useState, useEffect } from "react-router-dom"
+
+
+const Home = () => {
+
+    // const [news, setNews] = useState()
+
+
+    // useEffect(() => {
+    //     fetch()
+
+
+    // }, [])
+
+    return(
+        <>
+        
+        
+        </>
+    )
+
+}
+
+export default Home
