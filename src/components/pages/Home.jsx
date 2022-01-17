e15ebdc2478a22c798527436ec54c939ec9c737a
Diego Barboza <barbozadiego1994@gmail.com>
2022-01-16 23:07:31 -0400

    A custom SELECT is created for news categories

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { useLocation } from 'react-router-dom'
 
 import Header from '../Organisms/Header'
@@ -7,17 +7,21 @@ import GridNews from '../Templates/GridNews'
 import Loader from '../Molecules/Loader'
 
 import '../../styles/home.css'
+import Icon from '../Atoms/Icon'
 
 
 const Home = () => {
 
+    const select = useRef()
     const location = useLocation()
 
     const [news, setNews] = useState()
+    const [category, setCategory] = useState('Select your news')
     // const [favesNews, setFavesNews] = useState()
 
 
     useEffect(() => {
+        // fetch(`https://hn.algolia.com/api/v1/search_by_date?query=${category}&page=0`)
         fetch('https://hn.algolia.com/api/v1/search_by_date?query=reactjs&page=0')
         .then(res => res.json())
         .then(data => {
@@ -40,13 +44,26 @@ const Home = () => {
 
     }, [])
 
+    const selectCategory = () => {
+        select.current.classList.toggle('display-picker')
+    }
+
+    const changeCategory = (e) => setCategory(e.target.textContent)
+
+
     return (
         <>
          <Header />
          <MainMenu />
-         <section>
+         <section className='main-container'>
             <div className="news-picker">
-
+                <div onClick={selectCategory} >{category}</div>
+                <ul ref={select}>
+                    <li onClick={changeCategory}><img src="/images/angular.png" alt="angular-logo" /> angular</li>
+                    <li onClick={changeCategory}><img src="/images/reactjs.png" alt="react-logo" /> reactjs</li>
+                    <li onClick={changeCategory}><img src="/images/vuejs.png" alt="vue-logo" /> vuejs</li>
+                </ul>
+                <span onClick={selectCategory} className='arrow-down'><Icon tags='arrow-down'/></span>
             </div>
 
             {
@@ -63,6 +80,8 @@ const Home = () => {
                      <Loader message='Getting data from server...' />  
                    </div>
             }
+
+            {category}
              
          </section>
         </>
